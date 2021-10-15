5a5a78e4aaddaf9d41936c5cce432429415710b0
Phil Owen <phillipsowen@outlook.com>
2021-10-15 15:35:48 -0400

    tidying up comments

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-# from gunicorn.app.wsgiapp import WSGIApplication
-#
-# # --bind 0.0.0.0:8080 -w 1 -k uvicorn.workers.UvicornWorker -t 600 src.server:APP
-# app = WSGIApplication()
-#
-# app.run()
-
 import uvicorn
 
 class App:
