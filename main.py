782a24b704152bb43821d2fdbc795df2e49a6535
Phil Owen <phillipsowen@outlook.com>
2021-10-25 10:06:27 -0400

    adjusting host address for container

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ class App:
 app = App()
 
 if __name__ == "__main__":
-    uvicorn.run("src.server:APP", host="127.0.0.1", port=4000, log_level="info", workers=1)
+    uvicorn.run("src.server:APP", host="0.0.0.0", port=4000, log_level="info", workers=1)
