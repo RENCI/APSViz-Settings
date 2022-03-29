4bbb57630c1b6065c69d8ef5baa0f368e4680a6d
Phil Owen <phillipsowen@outlook.com>
2022-03-29 05:52:56 -0400

    upping version

--- src/server.py
+++ src/server.py
@@ -13,7 +13,7 @@ from common.logging import LoggingUtil
 from src.pg_utils import PGUtils
 
 # set the app version
-APP_VERSION = 'v0.0.3'
+APP_VERSION = 'v0.0.4'
 
 # get the log level and directory from the environment.
 # level comes from the container dockerfile, path comes from the k8s secrets
