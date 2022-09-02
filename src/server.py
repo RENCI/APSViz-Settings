5dcfa6241554c80b4a4f66a88b319ada868bf4f6
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2022-09-02 12:31:48 -0400

    adding the new ast run harvester process

--- src/server.py
+++ src/server.py
@@ -65,6 +65,7 @@ APP.add_middleware(
 class JobTypeName(str, Enum):
     adcirc2cog_tiff_job = 'adcirc2cog-tiff-job'
     adcirctime_to_cog_job = 'adcirctime-to-cog-job'
+    ast_run_harvester_job = 'ast-run-harvester-job'
     # compute_mbtiles_job_0_10 = 'compute-mbtiles-job-0-10'
     # compute_mbtiles_job_11 = 'compute-mbtiles-job-11'
     # compute_mbtiles_job_12 = 'compute-mbtiles-job-12'
@@ -83,6 +84,7 @@ class JobTypeName(str, Enum):
 class NextJobTypeName(str, Enum):
     adcirc2cog_tiff_job = 'adcirc2cog-tiff-job'
     adcirctime_to_cog_job = 'adcirctime-to-cog-job'
+    ast_run_harvester_job = 'ast-run-harvester-job'
     complete = 'complete'
     # compute_mbtiles_job_0_10 = 'compute-mbtiles-job-0-10'
     # compute_mbtiles_job_11 = 'compute-mbtiles-job-11'
@@ -120,6 +122,7 @@ image_repo_to_repo_name: dict = {
 job_type_to_image_name: dict = {
     'adcirc2cog-tiff-job': '/adcirc2cog:',
     'adcirctime-to-cog-job': '/adcirctime2cogs:',
+    'ast-run-harvester-job': '/ast_run_harvester',
     # 'compute-mbtiles-job-0-10': '/adcirc2mbtiles:',
     # 'compute-mbtiles-job-11': '/adcirc2mbtiles:',
     # 'compute-mbtiles-job-12': '/adcirc2mbtiles:',
@@ -138,6 +141,7 @@ job_type_to_image_name: dict = {
 job_type_name_to_id: dict = {
   "adcirc2cog-tiff-job": 23,
   'adcirctime-to-cog-job': 26,
+  'ast-run-harvester-job': 27,
   "complete": 21,
   # "compute-mbtiles-job-0-10": 16,
   # "compute-mbtiles-job-11": 17,
