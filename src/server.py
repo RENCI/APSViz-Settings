07c3c7247eefeeb5f3035ec70319a6071fb0644b
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2023-03-24 11:19:43 -0400

    adding load-geoserver-s3 handling

--- src/server.py
+++ src/server.py
@@ -118,12 +118,14 @@ image_repo_to_repo_name: dict = {'renciorg': 'renciorg', 'containers.renci.org':
 # declare the component job type image name
 job_type_to_image_name: dict = {'adcirc2cog-tiff-job': '/adcirc2cog:', 'adcirctime-to-cog-job': '/adcirctime2cogs:',
                                 'ast-run-harvester-job': '/ast_run_harvester', 'final-staging-job': '/stagedata:', 'geotiff2cog-job': '/adcirc2cog:',
-                                'hazus': '/adras:', 'load-geo-server-job': '/load_geoserver:', 'obs-mod-ast-job': '/ast_supp:',
+                                'hazus': '/adras:', 'load-geo-server-job': '/load_geoserver:', 'load-geo-server-s3-job': '/load_geoserver:',
+                                'obs-mod-ast-job': '/ast_supp:',
                                 'staging': '/stagedata:'}
 
 # declare job name to id
 job_type_name_to_id: dict = {"adcirc2cog-tiff-job": 23, 'adcirctime-to-cog-job': 26, 'ast-run-harvester-job': 27, "complete": 21,
-                             "final-staging-job": 20, "geotiff2cog-job": 24, "hazus": 12, "load-geo-server-job": 19, "obs-mod-ast-job": 25,
+                             "final-staging-job": 20, "geotiff2cog-job": 24, "hazus": 12, "load-geo-server-job": 19, "load-geo-server-s3-job": 28,
+                             "obs-mod-ast-job": 25,
                              "staging": 11}
 
 
